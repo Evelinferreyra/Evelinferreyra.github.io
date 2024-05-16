37b435c89e45c76a78ca9c64f61b4576f91ace50
cele romero <115805225+Celeromero0808@users.noreply.github.com>
2024-05-16 13:27:17 -0300

    hola eve

---
+++ potencias.js
@@ -0,0 +1,20 @@
+function calculatePower() {
+    var power = document.getElementById('power').value;
+    var result = '';
+    switch (Math.abs(power) % 4) {
+      case 0:
+        result = '1';
+        break;
+      case 1:
+        result = 'i';
+        break;
+      case 2:
+        result = '-1';
+        break;
+      case 3:
+        result = '-i';
+        break;
+    }
+    document.getElementById('result').innerText = 'El resultado es: ' + result;
+  }
+  
