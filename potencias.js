2f8421a4063e4e2b540d15ef11a1697995f65f79
Evelinferreyra <115805581+Evelinferreyra@users.noreply.github.com>
2024-06-18 14:47:53 -0300

    cele
    cele
    
    Co-Authored-By: Celeromero0808 <115805225+Celeromero0808@users.noreply.github.com>

--- potencias.js
+++ potencias.js
@@ -1,21 +1,14 @@
 
-  function calculatePower() {
-    var power = document.getElementById('power').value;
-    var result = '';
-    switch (Math.abs(power) % 4) {
-      case 0:
-        result = '1';
-        break;
-      case 1:
-        result = 'i';
-        break;
-      case 2:
-        result = '-1';
-        break;
-      case 3:
-        result = '-i';
-        break;
-    }
-  
-    document.getElementById('result').innerText = 'El resultado es: ' + result;
+function calculatePower() {
+  var power = document.getElementById('power').value;
+  var result = '1'; // Valor por defecto para cualquier potencia de 0
+  var powersOfI = ['1', 'i', '-1', '-i'];
+
+  // Ajustamos para potencias negativas y calculamos el resultado
+  power = parseInt(power, 10);
+  if (!isNaN(power)) {
+    result = powersOfI[((power % 4) + 4) % 4];
   }
+
+  document.getElementById('result').innerText = 'El resultado es: ' + result;
+}
