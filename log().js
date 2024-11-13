054704a13013625ee15b5268e34eeed31c420d4d
IanButcher <115805201+IanButcher@users.noreply.github.com>
2024-11-12 21:17:30 -0300

    Convertir a exponente

---
+++ log().js
@@ -0,0 +1,6 @@
+const base = document.querySelector('')
+const expo = document.querySelector('')
+const text = document.querySelector('')
+
+text.innerHTML = `${base} <sup>${}</sup> = ${expo}`
+text.innerHTML = `log<sub>${base}</sub>(${argument}) = ${result}`
